5532beaa0f18c1285d9ccdb604dbceabc53b1c1a
MANK1NG <ivan.ipr.03@gmail.com>
2024-10-14 18:26:32 +0200

    Cambios
    Puesto el enlace a github, puesto el canvas para el proyecto

--- src/script.js
+++ src/script.js
@@ -1,13 +1,21 @@
 // Archivo JavaScript donde puedes inicializar el canvas
-new Phaser.Game({
-    type: Phaser.AUTO,
-    width: 800,
-    height: 400,
-    scene: [ {
-      create: create
-    }]
-});
+let z = 1; let x = 3 / z;
 
-function create() {
-  this.add.text(400,200, "It works!");
-}
+const config ={
+  title: 'Stick-Do',
+  type :Phaser.CANVAS,
+  canvas: document.getElementById('juego'),
+  scale: {
+      width: Math.floor((360 * x)  / 1488 * window.innerWidth),
+      height: Math.floor((189 * x) / 783 * window.innerHeight),
+      zoom: z
+  },
+  width: 600,
+  height: 400,
+  parent: 'container',
+  backgroundColor:'#423423'
+  
+};
+
+
+const game = new Phaser.Game(config);
