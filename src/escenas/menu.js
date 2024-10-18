0213450fc24e52c7d774113c96d75774d20ee6d6
MANK1NG <ivan.ipr.03@gmail.com>
2024-10-18 16:08:57 +0200

    PrimeraEscena
    Cambiado el canvas, se muestra la primera escena sin nada

---
+++ src/escenas/menu.js
@@ -0,0 +1,15 @@
+
+export default class Menu extends Phaser.Scene{
+
+
+    constructor(){
+        super({key: 'menu'});
+    }
+    preload(){
+
+    }
+
+    create(){
+        console.log("me he creado");
+    }
+}
--- src/script.js
+++ src/script.js
@@ -1,21 +1,33 @@
 // Archivo JavaScript donde puedes inicializar el canvas
+import Menu from './escenas/menu.js'
+
 let z = 1; let x = 3 / z;
 
 const config ={
   title: 'Stick-Do',
   type :Phaser.CANVAS,
-  canvas: document.getElementById('juego'),
+  canvas: document.getElementById("juego"),
   scale: {
+    
       width: Math.floor((360 * x)  / 1488 * window.innerWidth),
       height: Math.floor((189 * x) / 783 * window.innerHeight),
       zoom: z
   },
-  width: 600,
-  height: 400,
-  parent: 'container',
-  backgroundColor:'#423423'
-  
+ 
+  scene: [Menu],
+  physics: {  
+    default: 'arcade', 
+    arcade: { 
+        gravity: { y: 200 }, 
+        debug: true
+    },
+    checkCollision: {
+        up: true,
+        down: true,
+        left: true,
+        right: true
+    }
+  },
 };
 
-
-const game = new Phaser.Game(config);
+new Phaser.Game(config);
