4af23b4c81aba360e2437fe1edc5cbcaf2e2d4b0
Ivan Palomino <ivan.ipr.03@gmail.com>
2024-10-26 00:03:56 +0200

    TileMap
    Pues tileMap con colisiones

--- src/escenas/templo.js
+++ src/escenas/templo.js
@@ -7,24 +7,42 @@ export default class Templo extends Phaser.Scene{
         super({key: 'templo'});
     }
     preload(){
-        this.load.image('templo', './assests/templo.png');
+        this.load.image('temploFondo', './assests/templo2.png');
 
         this.load.image('personaje', '../../img/personaje.png');
 
         this.load.image('katana', '../../img/katana.png');
 
         this.load.image('sai', '../../img/sai.png');
+
+
+        this.load.tilemapTiledJSON('templo', './assests/templo.json');
+        this.load.image('cuboNegro','./assests/cuboNegro.png')
     }
 
     create(){
         console.log("me he creado templo");
+       this.add.image(0, 0, 'temploFondo').setOrigin(0, 0);
 
-        this.add.image(0, 0, 'templo').setOrigin(0, 0);
+        this.map = this.make.tilemap({
+            key: 'templo',
+            tileWidth: 32,
+            tileHeight: 32
+        });
+        
+        const tileset1 = this.map.addTilesetImage('cuboNegro', 'cuboNegro');
+       this.backgroundLayer = this.map.createLayer('Capa de patrones 1', tileset1);            
+       this.backgroundLayer.setCollisionByProperty({ colision: true });
+
 
         let personaje = new Personaje(this, 50, 0, Personaje.WeaponType.KATANA, {keyUp: 'W', keyDown: 'S', keyLeft: 'A', keyRight: 'D', keyAttack: 'V'});
         let personaje2 = new Personaje(this, 50, 0, Personaje.WeaponType.SAI, {keyUp: 'up', keyDown: 'down', keyLeft: 'left', keyRight: 'right', keyAttack: 'P'});
+       
+        this.physics.add.collider(personaje, this.backgroundLayer);
+        this.physics.add.collider(personaje2, this.backgroundLayer);
+        
         //Crear mapa, cada suelo.create hace un objeto en esa posicion, setSacle su tamaño, collider para que no atraviese.
-        const suelo = this.physics.add.staticGroup();
+       /* const suelo = this.physics.add.staticGroup();
 
         const sueloIzq = suelo.create(244, 700); 
     
@@ -35,6 +53,7 @@ export default class Templo extends Phaser.Scene{
         sueloDcha.setScale(14, 4.4).refreshBody(); 
     
         this.physics.add.collider(personaje, suelo); 
-        this.physics.add.collider(personaje2, suelo); 
+        this.physics.add.collider(personaje2, suelo);*/ 
     }
 }
+
