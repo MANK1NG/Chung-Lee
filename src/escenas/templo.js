c6a9899298dedec74ee7f9a8a2716f6743ca526c
MANK1NG <ivan.ipr.03@gmail.com>
2024-11-12 15:23:25 +0100

    CambiosRuta

--- src/escenas/templo.js
+++ src/escenas/templo.js
@@ -11,12 +11,12 @@ export default class Templo extends Phaser.Scene{
     preload(){
         this.load.image('temploFondo', './assests/templo21.png');
 
-        this.load.spritesheet('personaje1', '../KatanaAnim/SpriteSheet_Katana_N.png', {
+        this.load.spritesheet('personaje1', './KatanaAnim/SpriteSheet_Katana_N.png', {
             frameWidth: 525,  // Ancho de cada fotograma
             frameHeight: 460  // Alto de cada fotograma
         });
 
-        this.load.spritesheet('personaje2', '../KatanaAnim/SpriteSheet_Katana_R.png', {
+        this.load.spritesheet('personaje2', './KatanaAnim/SpriteSheet_Katana_R.png', {
             frameWidth: 525,  // Ancho de cada fotograma
             frameHeight: 460  // Alto de cada fotograma
         });
