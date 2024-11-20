dc6096f2ee470cd9edfce4d6f1021f9e2a1a8abd
julepage <julepage@ucm.es>
2024-11-20 01:01:41 +0100

    Todas animaciones
    Daño, ataquesalto, potenciado, ''run,''hit y metodos de ataque

--- src/armas/sai.js
+++ src/armas/sai.js
@@ -3,7 +3,7 @@ export default class Sai extends Phaser.Physics.Arcade.Sprite{
         super(scene, x, y,'sai');// Llama al constructor de Phaser.Physics.Arcade.Sprite
         // this.daño = daño;
         // this.rango = rango;
-        
+        this.tamSprite = 140; //ancho sprite
         this.setScale(0.3);//escala del sprite 
         //this.setOrigin() su posicion
         
@@ -15,17 +15,36 @@ export default class Sai extends Phaser.Physics.Arcade.Sprite{
 
         //colisiones
         this.body.setSize(170,230);
-        if(personaje.flipX){ //si el sprite mira a la izq
+        if(personaje.flipX){ //si el sprite mira a la drch
             this.body.setOffset(10,150);//ajuste hitbox(x,y) si es (+) mueve a la drch y viceversa (en y arriba(-) y abajo(+))
         }
-        else{//si mira a la drch
-            this.body.setOffset(-140, 150);
+        else{//si mira a la izq
+            this.body.setOffset(-this.tamSprite, 150);
         }
         this.body.enable = true; //activa cuerpo fisico del obj para que detecte colisiones
+
+        if (personaje.anims.currentAnim && !personaje.body.blocked.down) { // si esta en el aire hace la hitbox arriba
+            this.body.setSize(170,490);
+            if(personaje.flipX){//drch
+                this.body.setOffset(0, -110);
+            } 
+            else{//mira izq
+                this.body.setOffset(-this.tamSprite, -110);
+            }          
+        }
+      
     }
 
     potenciatedAttack(personaje){
-
+        this.scene.physics.add.existing(this);// le mete fisicas al sprite
+        this.body.setAllowGravity(false); // quitamos gravedad
+        this.body.setSize(170,325);
+        if(personaje.flipX){//drch
+            this.body.setOffset(10, 50)  
+        } 
+        else{//mira izq
+            this.body.setOffset(-this.tamSprite, 50);
+        }    
     }
 
     getAnimationConfig(personaje){
@@ -65,12 +84,40 @@ export default class Sai extends Phaser.Physics.Arcade.Sprite{
             repeat: -1,
            },
 
-        //    ataqueAire: {
-        //     key: "ataqueAire",
-        //     frames: this.anims.generateFrameNumbers(personaje.spriteSheetKey, {start: 80, end: 89} ),
-        //     frameRate: 20,
-        //     repeat: 0,
-        //    },
+           ataqueAire: {
+            key: "ataqueAire",
+            frames: this.anims.generateFrameNumbers(personaje.spriteSheetKey, {start: 80, end: 89} ),
+            frameRate: 20,
+            repeat: 0,
+           },
+
+           knockBack: {
+            key: 'knockBack',
+            frames: this.anims.generateFrameNumbers(personaje.spriteSheetKey, {start: 96 ,end: 110} ),
+            frameRate: 20,
+            repeat: 0,
+           },
+
+           ataquePotenciado: {
+            key: 'ataquePotenciado',
+            frames: this.anims.generateFrameNumbers(personaje.spriteSheetKey, {start: 112,end: 121} ),
+            frameRate: 20,
+            repeat: 0,
+           },
+
+           ataquePotenciadoRun: {
+            key: 'ataquePotenciadoRun',
+            frames: this.anims.generateFrameNumbers(personaje.spriteSheetKey, {start: 112,end: 121} ),
+            frameRate: 20,
+            repeat: 0,
+           },
+
+           ataquePotenciadoHit: {
+            key: 'ataquePotenciadoHit',
+            frames: this.anims.generateFrameNumbers(personaje.spriteSheetKey, {start: 128,end: 140} ),
+            frameRate: 20,
+            repeat: 0,
+           }
 
         }
     }
